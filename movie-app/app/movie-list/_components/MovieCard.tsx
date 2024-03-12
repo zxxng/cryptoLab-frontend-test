480a7c992b462b642a31ee04b3bdfddab90c53a5
zxxng <jeayeong123@naver.com>
2024-03-12 21:42:04 +0900

    fix: api 데이터 형식 오류 예외처리 추가

--- movie-app/app/movie-list/_components/MovieCard.tsx
+++ movie-app/app/movie-list/_components/MovieCard.tsx
@@ -27,31 +27,50 @@ const MovieCard = ({ movieData }: MovieCardProps) => {
     return <span>{stars}</span>
   }
 
+  const validateMovieData = (data: trendingMovie) => {
+    if (
+      !data.poster_path ||
+      !data.title ||
+      !data.release_date ||
+      !data.vote_average ||
+      !data.genre_ids ||
+      !data.overview
+    )
+      return false
+    return true
+  }
+
   return (
-    <article
-      className="w-[300px] h-[420px] rounded-lg"
-      style={{
-        backgroundImage: `url(https://image.tmdb.org/t/p/w500${movieData.poster_path})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-      }}
-    >
-      <div className="flex flex-col justify-between h-full p-4 rounded-lg bg-black bg-opacity-60 text-white opacity-0 hover:opacity-100 duration-300">
-        <h3 className="text-center font-suit text-base">{movieData.title}</h3>
-        <p className="text-center mb-28 font-suit text-sm">
-          {formatReleaseDate(movieData.release_date)}
-        </p>
-        <div className="flex flex-col gap-1 text-sm">
-          <p className="flex gap-1">
-            {movieData.vote_average.toFixed(2)}
-            {convertRatingToStars(movieData.vote_average)}
-          </p>
-          <p>{`genres: ${movieData.genre_ids.join(', ')}`}</p>
-          <p className="line-clamp-[8] mb-1">{movieData.overview}</p>
-          <button className="w-full text-right">View Details</button>
-        </div>
-      </div>
-    </article>
+    <>
+      {validateMovieData(movieData) ? (
+        <article
+          className="w-[300px] h-[420px] rounded-lg shadow-inner"
+          style={{
+            backgroundImage: `url(https://image.tmdb.org/t/p/w500${movieData.poster_path})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        >
+          <div className="flex flex-col justify-between h-full p-4 rounded-lg bg-black bg-opacity-60 text-white opacity-0 hover:opacity-100 duration-300">
+            <h3 className="text-center font-suit text-base">
+              {movieData.title}
+            </h3>
+            <p className="text-center mb-28 font-suit text-sm">
+              {formatReleaseDate(movieData.release_date)}
+            </p>
+            <div className="flex flex-col gap-1 text-sm">
+              <p className="flex gap-1">
+                {movieData.vote_average.toFixed(2)}
+                {convertRatingToStars(movieData.vote_average)}
+              </p>
+              <p>{`genres: ${movieData.genre_ids.join(', ')}`}</p>
+              <p className="line-clamp-[8] mb-1">{movieData.overview}</p>
+              <button className="w-full text-right">View Details</button>
+            </div>
+          </div>
+        </article>
+      ) : null}
+    </>
   )
 }
 
